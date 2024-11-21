a44623bc855ed72e42edad82e9340689316b3a4f
Soundar-techie <vksoundar2020@gmail.com>
2024-11-21 20:18:43 +0530

    calculator

---
+++ calcu.js
@@ -0,0 +1,28 @@
+const input = document.getElementById("values");
+
+function valueofnumbers(num){
+    input.value += num;
+}
+
+function AllClear(){
+    input.value ="";
+}
+
+function Delete(){
+
+    if(input.value === Number()){
+    input.value = input.value.slice(0,-1);
+    }
+    else{
+        input.value = "";
+    }
+}
+
+function sumof(){
+    try{
+        input.value = eval(input.value)
+    }
+    catch{
+        input.value = "Text Erorr";
+    }
+}
